c2f874a5baeb2c56e00e1ee794751b547faf04f3
gmp26 <gmp26@cam.ac.uk>
2016-01-05 07:38:13 +0000

    added dot generation

--- src/mystic/components.cljs
+++ src/mystic/components.cljs
@@ -3,6 +3,12 @@
    [rum.core :as rum]
    ))
 
+(def mystic-r 190)
+(def mystic-o {:x 200 :y 200})
+
+(defn dot-coord [key theta]
+  (+ (key mystic-o) (* mystic-r ((if (= key :x) Math.cos Math.sin) theta))))
+
 (rum/defc start-button [handler]
   [:button.start.btn-default.btn-left
    {:on-click handler
@@ -13,11 +19,13 @@
    {:on-click handler
     :on-touch-start handler} "Reset"])
 
-(rum/defc dot [r theta]
-  [:circle {:stroke "white" :stoke-width 3 :fill black :r 3
-            :cx (+ 200 (* r (Math.cos theta)))
-            :cy (+ 200 (* r (Math.sin theta)))}])
+(rum/defc dot [theta]
+  [:circle {:stroke "#ffffff" :stoke-width 5 :fill "#CCCCCC" :r 4
+            :cx (dot-coord :x theta)
+            :cy (dot-coord :y theta)}])
 
+
+(def mystic-n 50)
 
 (rum/defc mystic-rose []
   [:div
@@ -26,7 +34,7 @@
    [:input.inp-default {:type "number" :min 5 :max 100}]
    [:svg {:view-box "0 0 400 400"}
     [:g
-     [:circle.outlined {:fill "none" :stroke "black" :stroke-width 3 :cx 200 :cy 200 :r 190}]
+     [:circle.outlined {:fill "none" :stroke "black" :stroke-width 2 :cx 200 :cy 200 :r 190}]
      [:g
-      (dots 10)]
+      (map-indexed #(rum/with-key (dot (/ (* 2 Math.PI %1) mystic-n)) %2) (range mystic-n))]
      ]]])
