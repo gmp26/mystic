6263b64a7d4e645cfdbd616fccd5e0fff375255b
gmp26 <gmp26@cam.ac.uk>
2016-01-13 10:59:44 +0000

    separate draw sliders - not yet positioned

--- src/mystic/components.cljs
+++ src/mystic/components.cljs
@@ -5,7 +5,7 @@
    ))
 
 (def min-nodes 2)
-(def max-nodes 20)
+(def max-nodes 12)
 (def mystic-r 190)
 (def mystic-o {:x 200 :y 200})
 (def t-steps 100)
@@ -36,10 +36,13 @@
            :on-change #(swap! core/model assoc
                               :mystic-n (-> % .-target .-value))}])
 
-(rum/defc t-slider < rum/static [model t]
+(rum/defc t-slider < rum/static [model method t]
   [:input {:type "range" :value (* t-steps t) :min 0 :max t-steps
            :style {:width "100%"}
-           :on-change #(swap! model assoc :mystic-t (/ (-> % .-target .-value) t-steps))}])
+           :on-change #(swap! model
+                              (fn [old] (assoc old
+                                              :mystic-t (assoc (:mystic-t old)
+                                                               (dec method) (/ (-> % .-target .-value) t-steps)))))}])
 
 (rum/defc count-input < rum/cursored [model mystic-n]
   [:span.node-count
@@ -69,8 +72,9 @@
             :x2 (+ (* (dot-coord :x theta1) (- 1 t)) (* x2 t))
             :y2 (+ (* (dot-coord :y theta1) (- 1 t)) (* y2 t))
             :stroke "rgba(0,128,255,0.3)" ;"#08f"
-            :stroke-width 1
-            :marker-end (if (< t 1) "url(#arrow)" "none")}]))
+            :stroke-width 2
+            :marker-end (if (< t 1) "url(#arrow)" "none")
+            }]))
 
 (defn isCursor?
   "This at least distinguishes cursors from literal numbers .toString returns [object Object] for a Cursor"
@@ -79,7 +83,7 @@
 
 (rum/defc chords [mystic-n mystic-t method]
   (let [t (if (isCursor? mystic-t)
-            (if (= method 1) (/ @mystic-t 1) @mystic-t) 1)]
+            (@mystic-t (dec  method)) 1)]
     [:g
      (for [i (range @mystic-n)
            j (range (if (#{1 2} method) @mystic-n (if (= method 4) (- @mystic-n i) (inc i))))
@@ -95,12 +99,12 @@
          (= method 3) (rum/with-key (chord method i-theta j-theta (ramp (/ (dec i) @mystic-n) (/ 1 @mystic-n) t)) [3 i j])
          (= method 4) (rum/with-key (chord method i-theta j-theta' (ramp (/ i @mystic-n) (/ 1 @mystic-n) t)) [4 i j])))]))
 
-(rum/defc draw [model t-cursor]
+(rum/defc draw [model method t-cursor]
   [:#methods
    [:div {:key 1
           :style {:margin-top "10px"}} "Draw"]
    [:div {:key 2}
-    (t-slider model @t-cursor)]])
+    (t-slider model method @t-cursor)]])
 
 (rum/defc basic-mystic-rose < rum/cursored rum/cursored-watch [model]
   [:div
@@ -110,18 +114,18 @@
      [:circle.outlined {:fill "none" :stroke "black" :stroke-width 2 :cx 200 :cy 200 :r 190}]
      (dots-on-circle (rum/cursor model [:mystic-n]))
      (chords (rum/cursor model [:mystic-n]) 1 2)]]
-   ; [:p (str @model)]
 ])
 
-(rum/defc mystic-rose < rum/cursored rum/cursored-watch [model method]
+(rum/defc mystic-rose
+  [model method]
   [:div {:style {:display "inline-block" :width "50%"}}
    [:strong (str "Method " method)]
    [:svg {:view-box "0 0 400 400"}
     [:defs
      [:marker {:id "arrow"
-               :view-box "-1 -1 2 2"
+               :view-box "-0.5 -0.5 1 1"
                }
-      [:circle {:cx -2 :cy -2 :r 10 :fill "black"}]]]
+      [:circle {:cx 0 :cy 0 :r 0.3 :fill "black"}]]]
     [:g
      [:circle.outlined {:fill "none" :stroke "black" :stroke-width 2 :cx 200 :cy 200 :r 190}]
      (dots-on-circle (rum/cursor model [:mystic-n]))
@@ -130,7 +134,10 @@
 
 (rum/defc side-by-side < rum/cursored rum/cursored-watch [model]
   [:div
-   (draw model (rum/cursor model [:mystic-t]))
+   (draw model 1 (rum/cursor model [:mystic-t 0]))
+   (draw model 2 (rum/cursor model [:mystic-t 1]))
+   (draw model 3 (rum/cursor model [:mystic-t 2]))
+   (draw model 4 (rum/cursor model [:mystic-t 3]))
    (count-input model (rum/cursor model [:mystic-n]))
    [:div {:style {:clear "both"}}
     (mystic-rose model 1) (mystic-rose model 2)]
